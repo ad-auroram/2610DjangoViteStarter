4df60ff0dc436a012daed56ca21d2de25b755088
rseeley <roxseeley@gmail.com>
2024-11-17 16:18:55 -0700

    organizing home page

---
+++ client/src/components/main.jsx
@@ -0,0 +1,46 @@
+import './main.css'
+import { useState, useEffect } from 'react'
+
+export function Main(){
+    return (
+        <>
+        <h1>Home Page!</h1>
+            <div className='container'>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <img className='avatar' src='https://f2.toyhou.se/file/f2-toyhou-se/users/JustMoosh?2'></img>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Main;
