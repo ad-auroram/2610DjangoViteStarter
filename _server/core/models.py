6c69763ebb461ff881bb3adf140510a8ad225b69
rseeley <roxseeley@gmail.com>
2024-12-10 13:55:55 -0700

    styled the character page

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Session(models.Model):
+    token = models.TextField(unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Character(models.Model):
+    name= models.TextField()
+    review = models.TextField(blank=True)
+    rating = models.IntegerField(default=3)
+    share_publically = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Image(models.Model):
+    link = models.TextField(default="")
+    character =models.ForeignKey(Character, on_delete=models.CASCADE)
--- client/src/components/character.jsx
+++ client/src/components/character.jsx
@@ -1,13 +1,55 @@
 import './home.css'
+import './character.css'
 
 export function Character(){
     return(
         <>
-        <div>Name</div>
-        <div>Info</div>
-        <div>Avatar</div>
-        <div>Edit but only if char is theirs</div>
-        <div>Image gallery</div>
+        <div className='space'></div>
+        <div id='container'>
+            <div id='top'>
+                <img id='avatar' src='https://f2.toyhou.se/file/f2-toyhou-se/users/JustMoosh?2'></img>
+                <h1 id='name'>Name</h1>
+            </div>
+            <div id='info'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel ullamcorper metus. Maecenas euismod lectus eu consequat egestas. Fusce sed felis fringilla, mattis enim ac, ullamcorper ex. Donec eu egestas libero, sed facilisis neque. In ut facilisis metus. Nulla hendrerit velit ante, ac finibus mi hendrerit non. Vivamus ullamcorper convallis ex, ut dignissim ipsum tempor quis.
+
+Aliquam mi felis, tempus in elit nec, euismod pretium ante. Quisque ornare ornare elit. Quisque vitae faucibus lorem. Praesent a quam magna. Morbi ac felis augue. Aliquam gravida neque malesuada elit pellentesque accumsan. In eu justo eu justo tincidunt tristique. Curabitur hendrerit mi eget massa consectetur consequat. Sed auctor pulvinar turpis quis porttitor. Suspendisse interdum viverra finibus. Sed a viverra turpis, vel hendrerit dui. Mauris dignissim dolor ac lectus sodales, et convallis enim tincidunt.
+
+In interdum purus quis tellus pretium dignissim. Maecenas et bibendum erat. Morbi non purus leo. Maecenas congue, dolor id tincidunt condimentum, libero nibh tempus leo, ut rhoncus lectus orci efficitur magna. Aliquam at diam non metus gravida lacinia sit amet non mauris. Donec nisi dolor, cursus in quam et, accumsan condimentum est. Donec neque neque, aliquam eget sollicitudin a, luctus id velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur at nisi ac eros pellentesque sagittis pulvinar vitae nisl. Morbi semper in magna vestibulum aliquam. 
+            </div>
+            <button id='edit'>Edit</button>
+            <h2>Gallery</h2>
+            <div className='container'>
+                <div className='char-box'>
+                    <div className='title'>guy here</div>
+                    <div className='avatar'>woah info</div>
+                </div>
+                <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            <div className='char-box'>
+                <div className='title'>guy here</div>
+                <div className='avatar'>woah info</div>
+            </div>
+            </div>
+        </div>
         </>
     )
 }
